d0575e26a1a0bfb97db3da02055c895661901fa6
binary96 <gamindave1@gmail.com>
2022-12-14 01:35:51 +0100

    what the

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints out _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char putt = "_putchar";
+
+	putchar(putt);
+	return (0);
+}
