0822111adf3c758fadb51f72d597aaea255e6911
binary96 <gamindave1@gmail.com>
2022-12-14 01:50:01 +0100

    nanda

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
  * main - a program that prints out _putchar
  * Return: 0
  */
 int main(void)
 {
-	char putt = "_putchar";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	putchar(putt);
 	return (0);
 }
