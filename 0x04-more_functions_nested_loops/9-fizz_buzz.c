b423f42d7965490df98f1c03bbc4c440071a08b8
binary96 <gamindave1@gmail.com>
2022-12-17 04:06:49 +0100

    nobody

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints numbers from 1 to 100 and filters dem
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3) != 0 && (i % 5) != 0)
+		{
+			printf("%i ", i);
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		if ((1 % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
+
