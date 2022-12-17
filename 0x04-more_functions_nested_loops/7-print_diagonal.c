888ca6f21413e3bf5cd6878cd18c3212e2a01e6b
binary96 <gamindave1@gmail.com>
2022-12-17 02:42:27 +0100

    obara

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints out a line
+ * @n: the number of times the line should be printed
+ * Return:
+ */
+void print_line(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
