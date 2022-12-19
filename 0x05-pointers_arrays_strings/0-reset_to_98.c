be193061eab5046f0d75883288a918b6618561d2
binary96 <gamindave1@gmail.com>
2022-12-19 22:06:14 +0100

    what is dis

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value to 98
+ * @n: is the pointer variable
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - checks the lenght of a string
+ * @s: is pointer for str
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int p;
+
+	p = strlen(s);
+	return (p);
+}
