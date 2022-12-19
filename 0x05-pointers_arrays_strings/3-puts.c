26c93e745cd99ce823903626cb41fbfdcbfc61ba
binary96 <gamindave1@gmail.com>
2022-12-19 22:24:35 +0100

    magician

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - prints out a string followed by new line
+ * @str: is the string
+ * Return: no return value
+ */
+void _puts(char *str)
+{
+	 puts(str);
+}
