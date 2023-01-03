5c4a023406484f412f4b7e5f2c2bc57f7f18c794
binary96 <gamindave1@gmail.com>
2023-01-04 00:25:25 +0100

    work

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; i >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 		return (&s[i]);
 		}
 	}
-	return NULL;
+	return (0);
 }
