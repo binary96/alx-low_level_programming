35862072854e5151860000e3f41f6c26110d5a11
binary96 <gamindave1@gmail.com>
2023-01-04 23:20:42 +0100

    chass

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,13 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j, len;
 
-	len = sizeof(a)/sizeof(a[0][0]);
+	len = sizeof(a) / sizeof(a[0][0]);
 	for (i = 0; i < len; i++)
 	{
-		for (j = 0; j < len;j++)
+		for (j = 0; j < len; j++)
 		{
 		_putchar(a[i][j]);
-		if (j == len -1)
+		if (j == len - 1)
 		{
 			_putchar('\n');
 		}
