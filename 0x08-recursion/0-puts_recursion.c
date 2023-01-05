c2c8876932f09cebb2aac1ff00cbb36402c7327a
binary96 <gamindave1@gmail.com>
2023-01-05 01:02:02 +0100

    small

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - entry point
+ * @s: input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+
+/*	len = strlen(s);*/
+
+	_putchar(*s);
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+
+}
