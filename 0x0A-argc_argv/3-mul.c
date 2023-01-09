d75c2f81fa7873fdfa7c8fcca3a720c21416f677
binary96 <gamindave1@gmail.com>
2023-01-09 11:15:06 +0100

    sa

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, mul;
+	int mul;
 
-	i = 1;
-	if (i < argc)
+	if (argc > 1)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
-	printf("%d\n", mul);
+	else if (argc > 3 || argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	return (0);
 }
 
