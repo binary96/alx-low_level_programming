ce7935386a07abc9b746e8a1b0f22332f5b37ff3
binary96 <gamindave1@gmail.com>
2023-01-19 04:01:29 +0100

    shocked

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,34 +2,53 @@
 #include <stdio.h>
 /**
  * op_add - returns adds two integers
- * op_sub - returns subtracts two integers
- * op_mul - returns multiplies two integers
- * op_div - returns divides two integers
- * op_mod - function returns the remainder of division of two integers
  * @a: int input
  * @b: int input
+ * Return: the addition of a and b
 */
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - returns subtracts two integers
+ * @a: input
+ * @b: input
+ * Return: the subtraction of a and b
+ */
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: the product of a and b
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number
+ * Return: the division on a by b
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
+/**
+ * op_mod - Returns the remainder of division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: the remainder of a divided by b
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
